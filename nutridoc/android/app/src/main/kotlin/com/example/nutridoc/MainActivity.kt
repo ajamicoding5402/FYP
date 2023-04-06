709cff48bca98249dda6b46d5bd553a0d3fba19c
Ibrahim Ajami <ibrahi5402@gmail.com>
2023-04-07 00:26:35 +0800

    project has been created

---
+++ nutridoc/android/app/src/main/kotlin/com/example/nutridoc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nutridoc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ nutridoc/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('3eib'),
+      ),
+    );
+  }
+}
